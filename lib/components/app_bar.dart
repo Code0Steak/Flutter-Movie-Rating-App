e2cc584b77252b99949eb0e95dc384c6e0b5e8a7
Ameya Keskar <mycodestreak@gmail.com>
2023-09-26 02:24:10 +0530

    Implemented Logout

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -1,8 +1,9 @@
 // ignore: file_names
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-
-AppBar appBar() {
+AppBar appBar({User? user, Future<void> Function()? logout}) {
   return AppBar(
     title: const Text('Movies App',
       style:  TextStyle(
@@ -14,9 +15,25 @@ AppBar appBar() {
       backgroundColor: Colors.white,
       elevation: 0.9,
       centerTitle: true,
-  
+    actions: <Widget>[
+          if (user != null) 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(user.photoURL ?? ''), // User's profile image URL
+                radius: 20, // Adjust the size as needed
+              ),
+            ),
+            if (user != null) 
+            IconButton(
+            icon: const Icon(Icons.logout), // Add a logout icon
+            onPressed: (){
+              logout!();
+            }, 
+            color: HexColor("#444653"),// Call the logout callback when pressed
+          ),
+
+          // Add other action buttons here if needed
+        ],
   );
 }
-
-
-
